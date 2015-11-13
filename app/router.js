5f7a77a4ab40f038fd0bbf30183757797c0af57b
Naxum <jake@naxite.com>
2015-11-13 00:35:25 -0600

    Add support to scroll to top on transition

--- app/router.js
+++ app/router.js
@@ -2,7 +2,15 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  didTransition(transition) {
+      this._super(...arguments);
+      window.scrollTo(0, 0);
+      return ga('send', 'pageview', {
+        'page': this.get('url'),
+        'title': this.get('url')
+      });
+  }
 });
 
 Router.map(function() {
